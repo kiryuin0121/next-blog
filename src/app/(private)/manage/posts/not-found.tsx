0b62837b385417f5a19852d3dfbde1fbea4e50e0
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2025-05-13 10:08:31 +0900

    記事管理ページにnot-foundページを実装

---
+++ src/app/(private)/manage/posts/not-found.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen px-4 py-4 gap-8">
+      <div className="text-center">
+        <p className="text-2xl mb-4">お探しの記事が見つかりませんでした</p>
+        <p className="text-sm text-[#565656]">
+          記事が存在しないか、すでに削除された可能性があります。
+        </p>
+      </div>
+      <div>
+        <Button
+          asChild
+          className="border-2 py-4 px-4 hover:text-black hover:border-black hover:bg-[#19d1ff]"
+        >
+          <Link href={"/dashboard"}>記事管理ページへ戻る</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
